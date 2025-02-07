b0cecd6da28217aa8ac537b7de9ab3858073097b
mrghost077 <msnlms86@gmail.com>
2025-02-07 22:02:52 +0530

    Stateful widget - button

--- lib/main.dart
+++ lib/main.dart
@@ -2,265 +2,44 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(SecondApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SecondApp extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    return SecondAppState();
+  }
+}
+
+class SecondAppState extends State<SecondApp>{
+
+  String displayText = "Hello World";
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Restaurant Menu",
-          style: TextStyle(color: Colors.black),),
-          backgroundColor: Colors.orangeAccent,
+        appBar: AppBar(title: Text("Changing the text using Stateful Widget",
+        style: TextStyle(color: Colors.white),
         ),
-        body: ListView(
+        backgroundColor: Colors.blue,
+        ),
+        body: Column(
           children: [
-            Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Biriyani", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious Biriyani with egg and tandoori chicken"),
-              Text("Rs 800",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Chicken Noodles", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious Noodles with spices and tandoori chicken"),
-              Text("Rs 1000",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Chicken Rice", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious rice with vegetables and fried chicken"),
-              Text("Rs 659",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Chicken Koththu", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious kothhu with spices and fried chicken"),
-              Text("Rs 600",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Biriyani", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious Biriyani with egg and tandoori chicken"),
-              Text("Rs 800",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Chicken Noodles", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious Noodles with spices and tandoori chicken"),
-              Text("Rs 1000",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Chicken Rice", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious rice with vegetables and fried chicken"),
-              Text("Rs 659",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(spreadRadius: 2,
-              blurRadius: 10, 
-              offset: Offset(0, 5),
-              color: Colors.black26),
-            ]
-          ),
-          height: 90,
-          width: double.infinity,
-          margin: EdgeInsets.all(8),
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Chicken Koththu", style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold
-              ),),
-              Text("Delicious kothhu with spices and fried chicken"),
-              Text("Rs 600",style: TextStyle(
-                fontWeight: FontWeight.bold
-              ),),
-            ],
-          ),
-        )
+            Text(displayText),
+            MaterialButton(
+              onPressed: (){
+                setState(() {
+                  displayText = "Welcome to Ghost programing";
+                });
+              },
+              child: Text("Click to Change",
+              style: TextStyle(color: Colors.green),),
+              )
           ],
-        ),
+        )
       ),
     );
   }
